1b4c174dfb30ed97b6efaf492d0287a9b1394c4f
erikmoratrejo <erikmoratrejo@gmail.com>
2024-05-18 09:06:22 -0600

    🎞Guarfar picture

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,23 +1,54 @@
 <template>
-  <ion-page>
+   <ion-content>
+  <ion-grid>
+    <ion-row>
+      <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+        <ion-img :src="photo.webviewPath"></ion-img>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
+  <!-- <ion-fab> markup -->
+</ion-content>
+
+   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Fotografia</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+        <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large"></ion-title>
         </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 2 page" />
-    </ion-content>
+      </ion-header>  
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonContent,
+  IonCol,
+  IonFab,
+  IonFabButton,
+  IonGrid,
+  IonPage,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
+import { camera, trash, close } from 'ionicons/icons';
+const { photos, takePhoto } = usePhotoGallery();
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -2,15 +2,15 @@
   <ion-page>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar slot="fotos">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon aria-hidden="true" :icon="atCircle" />
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon aria-hidden="true" :icon="images" />
+          <ion-label>Photo</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
@@ -24,5 +24,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { atCircle, images, square,} from 'ionicons/icons';
 </script>
